d036399927d7394593131a54c8efdc670a8d448d
Ricardo Casallas <e@mail.com>
2014-02-16 09:27:05 -0500

    Robot project added

--- wind_river/.metadata/.plugins/com.windriver.ide.core/compilerInspection.c.d
+++ wind_river/.metadata/.plugins/com.windriver.ide.core/compilerInspection.c.d
@@ -1,2 +1,2 @@
 compilerInspection.o:  \
- D:/software/usfirst/2014/aerial_assist/.metadata/.plugins/com.windriver.ide.core/compilerInspection.c
+ D:/software/usfirst/2014/aerial_assist/wind_river/.metadata/.plugins/com.windriver.ide.core/compilerInspection.c
